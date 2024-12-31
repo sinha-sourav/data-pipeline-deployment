b950e892d65ede90b05b91c161f55ea82301f347
unknown <souravsinha.eco@gmail.com>
2024-12-31 11:20:56 -0500

    Exploration

--- src/exception.py
+++ src/exception.py
@@ -19,11 +19,11 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__=='__main__':
+# if __name__=='__main__':
 
-    try: 
-        a = 1/10 
-    except Exception as e:
-        logging.info('Logging has started')
-        raise CustomException(e, sys)
+#     try: 
+#         a = 1/0
+#     except Exception as e:
+#         logging.info('Logging has started')
+#         raise CustomException(e, sys)
         
